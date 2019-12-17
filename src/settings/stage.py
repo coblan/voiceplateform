57dca47d598f7807c88d6b4583d008eabda47fdd
he_yulin <he_yulin@163.com>
2019-12-17 16:49:28 +0800

    配置文件

--- src/settings/stage.py
+++ src/settings/stage.py
@@ -1,5 +1,18 @@
 from .base import *
 
+DATABASES = {
+     'default': {
+        'NAME': 'voiceplatform',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'root553',
+        'HOST': '127.0.0.1', 
+        'PORT': '3306', 
+        'OPTIONS': {'charset':'utf8mb4'},
+
+      },
+} 
+
 AGORA={
     'appID':'303156d224e44881a00af9cabc9e10d8',
     'appCertificate':'0ca642eb8e3a4c868822af7e4d0dfee9'
