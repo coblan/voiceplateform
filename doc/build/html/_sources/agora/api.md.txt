b22f7903a91bc71497696595b994bc5d7d7145f3
he_yulin <he_yulin@163.com>
2019-12-18 16:54:18 +0800

    debug

--- doc/build/html/_sources/agora/api.md.txt
+++ doc/build/html/_sources/agora/api.md.txt
@@ -12,7 +12,7 @@
 [GET]   /dapi/agora/token  
 ```
     
-### 获取option
+### RTC获取token
 
 根据声网接口文档，需要用到`appID,channel,uid,token`这些参数，现在可以由后端直接生成，返回给前端使用。
 
@@ -35,6 +35,31 @@
         "channel": "test_channel",
         "uid": 987654321,
         "token": "006303156d224e44881a00af9cabc9e10d8IAAxzfgl2eltoS+/UvNYWOA79m0bgaGryJzew8+iCUUfYY9auH4BAl8BEABxwigBpev5XQEAAQB9nPhd"
+    }
+}
+```
+    
+### RTM获取token
+
+根据声网接口文档，需要用到`appID,channel,uid,token`这些参数，现在可以由后端直接生成，返回给前端使用。
+
+```
+[GET]   /dapi/agora/rtm-option?channel=mychanel&uid=1235          
+
+```
+
+| 参数  | 含义  | 必填  | 默认值  |
+|-------|-------|-------|------|
+| uid | 用户唯一id | 否 |  987654321 |
+
+返回:
+``` javascript
+{
+    "success": true,
+    "data": {
+        "appID": "303156d224e44881a00af9cabc9e10d8",
+        "uid": "987654321",
+        "token": "006303156d224e44881a00af9cabc9e10d8IADW2XOcCfXR4zFGhvP+3QhDvarhYrQwgaIaRYHc1jUF2AECXwEAAAAAEACPKcoCMzr7XQEA6AML6/ld"
     }
 }
 ```
--- src/part3/Agora_interface.py
+++ src/part3/Agora_interface.py
@@ -2,6 +2,7 @@ from helpers.director.shortcut import director_view,doc_fun,doc_str
 from django.conf import settings
 import time
 from . Agora.RtcTokenBuilder import RtcTokenBuilder
+from . Agora.RtmTokenBuilder import RtmTokenBuilder
 
 doc_str('agora/api.md','''
 # 语音对接
@@ -36,7 +37,7 @@ def get_token():
 @director_view('agora/rtc-option')
 def get_option(uid=None,channel=None,):
     '''
-### 获取option
+### RTC获取token
 
 根据声网接口文档，需要用到`appID,channel,uid,token`这些参数，现在可以由后端直接生成，返回给前端使用。
 
@@ -73,6 +74,50 @@ def get_option(uid=None,channel=None,):
     return {
         'appID':appID,
         'channel':channelName,
+        'uid': userAccount,
+        'token':token,
+    }
+
+
+@doc_fun('agora/api.md')
+@director_view('agora/rtm-option')
+def get_option(uid=None,channel=None,):
+    '''
+### RTM获取token
+
+根据声网接口文档，需要用到`appID,channel,uid,token`这些参数，现在可以由后端直接生成，返回给前端使用。
+
+```
+[GET]   /dapi/agora/rtm-option?channel=mychanel&uid=1235          
+
+```
+
+| 参数  | 含义  | 必填  | 默认值  |
+|-------|-------|-------|------|
+| uid | 用户唯一id | 否 |  987654321 |
+
+返回:
+``` javascript
+{
+    "success": true,
+    "data": {
+        "appID": "303156d224e44881a00af9cabc9e10d8",
+        "uid": "987654321",
+        "token": "006303156d224e44881a00af9cabc9e10d8IADW2XOcCfXR4zFGhvP+3QhDvarhYrQwgaIaRYHc1jUF2AECXwEAAAAAEACPKcoCMzr7XQEA6AML6/ld"
+    }
+}
+```
+    '''
+    appID = settings.AGORA.get('appID')
+    appCertificate = settings.AGORA.get('appCertificate')
+    #channelName= channel  or 'test_channel'
+    userAccount= uid or '987654321'
+    Role_Rtm_User = 1
+    privilegeExpiredTs = time.time() + 600
+    token = RtmTokenBuilder.buildToken(appID, appCertificate, userAccount, Role_Rtm_User, privilegeExpiredTs)
+    return {
+        'appID':appID,
+        #'channel':channelName,
         'uid': userAccount,
         'token':token,
     }
