334f1d52a1f4d03ac294dffccaa916d45236a31f
he_yulin <he_yulin@163.com>
2020-03-13 00:27:12 +0800

    sss

--- script/filebeat.py
+++ script/filebeat.py
@@ -1,9 +1,10 @@
-from dfilebeat import DFileBeat,multi_tail_file,django_log_parsers,elastice_search
+from fastdog.maintain.filebeat.dfilebeat import DFileBeat,multi_tail_file,django_log_parsers,elastice_search
 from functools import partial
 
 pp = DFileBeat(harvest= partial(multi_tail_file,
                                    [
                                        '/pypro/voiceplatform/log/process.log',
+                                       '/pypro/voiceplatform/log/django.log'
                                    ]),
                   parsers =django_log_parsers,
                   outputs = [
