8b50b65c4b8c8a671525f9515d617d6267aa85a1
he_yulin <he_yulin@163.com>
2020-03-15 12:34:57 +0800

    debug关闭

--- src/settings/stage2.py
+++ src/settings/stage2.py
@@ -19,6 +19,8 @@ DATABASES = {
 
 ALLOWED_HOSTS=['liu.enjoyst.com','localhost']
 
+DEBUG =False
+
 AGORA={
     'appID':'303156d224e44881a00af9cabc9e10d8',
     'appCertificate':'0ca642eb8e3a4c868822af7e4d0dfee9'
